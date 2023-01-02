2e4bd5bd2d6df2b3d2a925f63e908b1449b1cf96
prabha1207 <98202075+prabha1207@users.noreply.github.com>
2023-01-02 15:38:26 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+<!-- <HeaderComponent/> -->
+  <router-view/>
+</template>
+<script lang="ts">
+// import HeaderComponent from "./components/HeaderComponent.vue";
+import {defineComponent} from "@vue/runtime-core"
+export default defineComponent({
+
+  name:'App',
+// created(){
+// console.log("app was created");
+
+// },
+//   mounted(){
+//     console.log("log from app mounted");
+    
+//   },
+//   components:{
+//     HeaderComponent
+//   }
+ 
+})
+</script>
+
+
+<style>
+
+
+</style>
